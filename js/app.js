01170c8cbdd874cf7d9641310a522401b77ec6d0
unknown <amebattcode>
2023-09-09 03:12:07 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let colorScope = document.getElementById('shape');
+colorScope.style.backgroundColor = 'red';
+let divPace = document.getElementById('happyGreeting');
+divPace.textContent = "Happy birthday";
+document.body.style.backgroundColor = 'green';
